387b6c94a42d4de6464bff3d5596c3cd4c836379
Dominik Kubis <kubis.dominik1@gmail.com>
2016-11-07 17:32:40 +0100

    [TASK-4] - setup gulp taks: scripts, clean, default, watch

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,46 @@ var merge = require('merge2');
 
 var modules = 'node_modules/';
 var stylesSrc = 'src/styles/';
-var stylesDist = 'dist/styles';
+var scriptsSrc = 'src/scripts/';
+var stylesDist = 'dist/styles/';
+var scriptsDist = 'dist/scripts/';
 
 gulp.task('styles', function() {
     var bootstrapStream;
     var customStyleStream;
 
-    bootstrapStream = sass(modules + 'bootstrap/scss/bootstrap.scss');
+    bootstrapStream = sass(modules + 'bootstrap/scss/bootstrap.scss')
+        .pipe(autoprefixer('last 2 version'));
 
-    customStyleStream = sass(stylesSrc + 'main.scss');
+    customStyleStream = sass(stylesSrc + 'main.scss')
+        .pipe(autoprefixer('last 2 version'));
 
     return merge(bootstrapStream, customStyleStream)
         .pipe(concat('style.css'))
         .pipe(gulp.dest(stylesDist))
         .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src([
+        modules + 'jquery/dist/jquery.js',
+        modules + 'bootstrap/dist/js/bootstrap.js',
+        scriptsSrc + '**/*.js'
+    ])
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest(scriptsDist))
+        .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('clean', function() {
+    return del([stylesDist + '*.css', scriptsDist + '*.js']);
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles', 'scripts');
+});
+
+gulp.task('watch', function() {
+    gulp.watch(stylesSrc + '**/*.scss', ['styles']);
+    gulp.watch(scriptsSrc + '**/*.js', ['scripts']);
 });
